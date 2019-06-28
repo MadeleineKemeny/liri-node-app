179328bdd3982309414333d81ddc1a23a9152613
Madeleine Kemeny <madeleinekemeny@gmail.com>
2019-06-28 13:36:18 -0500

    initial files

---
+++ keys.js
@@ -0,0 +1,6 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.358fbc62133e463b922cc35d73288921,
+  secret: process.env.7cc72a3dfb234e7a8976f87680b83de7
+};
---
+++ liri.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+//spotify 
+var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: <your spotify client id>,
+  secret: <your spotify client secret>
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
+
+//axios
+var axios = require("axios")
+
+// Make a request for a user with a given ID
+axios.get("/user?ID=12345")
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
