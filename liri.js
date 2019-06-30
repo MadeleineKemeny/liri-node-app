301792704ee70ddb9140362ea0be650bb8583a2c
Madeleine Kemeny <madeleinekemeny@gmail.com>
2019-06-30 10:46:41 -0500

    reordering code

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-//  some kind of prompt with instructions on how to structure a query for this program?
+var fs = require("fs");
 
 //  using moment to parse date
 var moment = require("moment");
@@ -6,65 +6,113 @@ var moment = require("moment");
 //  if concert
 var axios = require("axios");
 
+var Spotify = require("node-spotify-api");
+
+// read from .env
+require("dotenv").config();
+var keys = require("./keys.js");
+
+
 //  entering commands
 var action = process.argv[2];
 console.log(process.argv);
 
-var searchTermArr = process.argv.slice(3);
-var searchTerm = searchTermArr.join(" ");
 
 
+if (action === "do-what-it-says") {
 
-if (action === "concert-this") {
+  fs.readFile("random.txt", "utf8", function(error, random) {
 
-  axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp").then(
-    function (response) {
+    // log errors
+    if (error) {
+      return console.log(error);
+    }
   
-      if (response.data.length === 0) {
-        console.log("No records found.");
-      }
+    // log snark
+    console.log(random);
+
+    // TODO read the contents of string random, split by comma, determine action and searchTerm
+    // E.g. spotify-this-song,"I Want it That Way"
+    // var actionFromRandom = 
+    // var searchTerm = 
+
+    // Now run the seach
+    runSearch(actionFromRandom, searchTerm);
   
-      else {
-        for (var i = 0; i < response.data.length; i++) {
-          var event = response.data[i];
-          // displaying the venue name
-          console.log("The concert's venue is: " + event.venue.name);
-          // displaying the venue location
-          console.log("The venue location is: " + event.venue.city);
-          // displaying the event date
-          var datetime = moment(event.datetime).format("MM/DD/YYYY")
-          console.log("The concert date is: " + datetime + "\n");
+  });
+  
+  // * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands: 1. It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`; 2. Edit the text in random.txt to test out the feature for movie-this and concert-this.
+  
+}
+else {
+  // action is something other than "do-what-it-says"
+  // read search term from argv
+  var searchTermArr = process.argv.slice(3);
+  var searchTerm = searchTermArr.join(" ");
+  runSearch(action, searchTerm);
+}
+
+
+function runSearch(action, searchTerm) {
+
+  if (action === "concert-this") {
+
+    axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp").then(
+      function (response) {
+    
+        if (response.data.length === 0) {
+          console.log("No records found.");
+        }
+    
+        else {
+          for (var i = 0; i < response.data.length; i++) {
+            var event = response.data[i];
+            // displaying the venue name
+            console.log("The concert's venue is: " + event.venue.name);
+            // displaying the venue location
+            console.log("The venue location is: " + event.venue.city);
+            // displaying the event date
+            var datetime = moment(event.datetime).format("MM/DD/YYYY")
+            console.log("The concert date is: " + datetime + "\n");
+          }
         }
       }
-    }
-  );
+    );
+  }
+
+  else if (action === "spotify-this-song") {
+
+    searchTerm = searchTerm || "The Sign Ace of Base";
+
+    var spotify = new Spotify(keys.spotify);
+    
+    spotify
+      .search({ type: 'track', query: searchTerm })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+
+  }
+
+  else if (action === "movie-this") {
+    searchTerm = searchTerm || "Mr. Nobody";
+    console.log("Movie: " + searchTerm);
+  }
+
+
+  else {
+// redefine search please
+  }
 }
-
-else if (action === "spotify-this-song") {
-
-}
-
-else if (action === "movie-this") {
-
-}
-
-else if (action === "do-what-it-says") {
-
-}
-
-else {
-
-}
-// for .env
-// require("dotenv").config();
-// var keys = require("./keys.js");
-
 
 
 // concert.search(searchTerm)
 console.log(searchTerm);
 
-/*
+
 //  if song
 song.search(track)({type: 'track', query: 'USER INPUT' }, function(err, data){
   if (err) {
@@ -110,26 +158,6 @@ axios.get("http://www.omdbapi.com/" + title + "&apikey=trilogy").then(
   //    },
   //  *4: response.data.ratings.[{source: "rotten tomatoes", "value","PERCENTAGE???" }];
 
-
-  //  ERROR HANDLING  1. If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody', 2. Prompt: "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/. It's on Netflix!"
-
-
-// "do-what-it-says"; for reading from random.txt
-var fs = require("fs");
-
-fs.readFile("random.txt", "utf8", function(error, random) {
-
-  // log errors
-  if (error) {
-    return console.log(error);
-  }
-
-  // log snark
-  console.log(data);
-
-
-});
-
 // * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands: 1. It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`; 2. Edit the text in random.txt to test out the feature for movie-this and concert-this.
 
 
@@ -145,47 +173,5 @@ function addEntry() {
 }
 
 
-
-// spotify API
-var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
-
-
-var spotify = new Spotify({
-  id: 'your spotify client id',
-  secret: 'your spotify client secret'
-});
-
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-
-console.log(data);
-});
-
-//  axios
-var axios = require("axios")
-
-//  axios GET request
-axios.get("/user?ID=12345")
-  .then(function (response) {
-    //  handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    //  handle error
-    console.log(error);
-  })
-  .finally(function () {
-    //  always executed
-  });
-
-
-// bands in town:
- var artist= "pink" ;
- "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-
-
  // omdb:
- "http://www.omdbapi.com/?i=tt3896198&apikey=171d8e50" //should key be someplace else? */
+ "http://www.omdbapi.com/?i=tt3896198&apikey=171d8e50" //should key be someplace else? 
