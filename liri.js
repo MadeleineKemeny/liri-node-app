8afb02f2a8f3071cf51123e1de1d2ad0c2de1f8a
Madeleine Kemeny <madeleinekemeny@gmail.com>
2019-07-03 17:33:21 -0500

    proof of life

--- liri.js
+++ liri.js
@@ -36,7 +36,8 @@ function spotify(searchTerm) {
 if (searchTerm){
    song=searchTerm}
    else{
-    song="the sign";
+    song="The Sign, Ace of Base";
+    console.log("REALLY? Okay...")
    }
     var spotify = new Spotify(keys.spotify);
     
@@ -58,22 +59,25 @@ function concert(searchTerm) {
 axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp").then(
   function (response) {
     for (i=0;i<response.data.length;i++){
-      console.log(response.data[i].venue.name)
-      console.log(response.data[i].venue.city + " ," + response.data[i].venue.country)
+      console.log(searchTerm + " is playing:")
+      console.log("Venue name: " + response.data[i].venue.name)
+      console.log("Venue location: " + response.data[i].venue.city + " ," + response.data[i].venue.region)
       date=response.data[i].datetime.replace("T"," ");
       formattedDate=moment(date).format("MM/DD/YYYY");
-      console.log("date: "+formattedDate + "\n");
+      console.log("Date: "+formattedDate+"\n");
+      
     };
   }
 ).catch(function(err){
   axios.get("https://rest.bandsintown.com/artists/rolling+stones/events?app_id=codingbootcamp").then(
   function (response) {
     for (i=0;i<response.data.length;i++){
-      console.log(response.data[i].venue.name)
-      console.log(response.data[i].venue.city + " ," + response.data[i].venue.country)
+      console.log("Undecided? Maybe check out The Rolling Stones' current tour!")
+      console.log("Venue name: " + response.data[i].venue.name)
+      console.log("Venue location: " + response.data[i].venue.city + " ," + response.data[i].venue.region)
       date=response.data[i].datetime.replace("T"," ");
       formattedDate=moment(date).format("MM/DD/YYYY");
-      console.log("date: "+formattedDate + "\n");
+      console.log("Date: "+formattedDate + "\n");
     };
   }
 )
@@ -85,8 +89,8 @@ function movie(searchTerm) {
 
   axios.get("http://www.omdbapi.com/?t="+ searchTerm + "&y=&plot=short&apikey=trilogy").then(
     function(response) {
-    console.log("movie title: "+response.data.Title);
-    console.log("year of release: "+response.data.Year);
+    console.log("Movie title: "+response.data.Title);
+    console.log("Year of release: "+response.data.Year);
     console.log("IMDB rating: "+response.data.Ratings[0].Value);
     console.log("Rotten Tomatoes: "+response.data.Ratings[1].Value);
     console.log("Country: "+response.data.Country);
@@ -94,12 +98,13 @@ function movie(searchTerm) {
     console.log("Plot: "+response.data.Plot);
     console.log("Actors: "+response.data.Actors);
 
-}).catch(function(err){
+  })
+  .catch(function(err){
   axios.get("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy").then(
     function(response) {
-    console.log("You might like Mr. Nobdy! It's on Netflix.");
-    console.log("movie title: "+response.data.Title);
-    console.log("year of release: "+response.data.Year);
+    console.log("Not sure what to watch? You might like Mr. Nobdy! It's on Netflix.");
+    console.log("Movie title: "+response.data.Title);
+    console.log("Year of release: "+response.data.Year);
     console.log("IMDB rating: "+response.data.Ratings[0].Value);
     console.log("Rotten Tomatoes: "+response.data.Ratings[1].Value);
     console.log("Country: "+response.data.Country);
